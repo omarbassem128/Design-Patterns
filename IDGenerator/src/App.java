f5fefc5bde1bdd73b2c20ab8e289ff23dc96b970
Omar Bassem <omarbassem889@gmail.com>
2025-05-17 23:17:39 +0300

    Generator should be this I think

--- IDGenerator/src/App.java
+++ IDGenerator/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        IDGenerator stu1 = IDGenerator.getInstance();
-        IDGenerator stu2 = IDGenerator.getInstance();
+        IDGenerator generator = IDGenerator.getInstance();
+        String stu1id = generator.generateID();
+        System.out.println(stu1id);
     }
 }
