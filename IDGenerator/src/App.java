c62097e4266e82562ebd4142c18f9d443c7474d7
Omar Bassem <omarbassem889@gmail.com>
2025-05-17 23:06:32 +0300

    new format. will solve each design pattern question in a different folder while providing the description of the problem in Description.txt

---
+++ IDGenerator/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        IDGenerator stu1 = IDGenerator.getInstance();
+        IDGenerator stu2 = IDGenerator.getInstance();
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-![image](https://github.com/user-attachments/assets/74ea7bcd-5f69-498a-a28f-e75f806a178e)
-
